9a5d8a598817f88c69de7c869150849e220d56ee
chaukhacquocbao <bao.chau@netdevices.fr>
2023-06-18 12:50:13 +0200

    builded client keycloak theme - Sebastian Schedlbauer

---
+++ build_keycloak/start_keycloak_testing_container.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker rm keycloak-testing-container || true
+
+cd "/home/crow/projects/keycloakify-starter/build_keycloak"
+
+docker run \
+   -p 8080:8080 \
+   --name keycloak-testing-container \
+   -e KEYCLOAK_ADMIN=admin \
+   -e KEYCLOAK_ADMIN_PASSWORD=admin \
+   -e JAVA_OPTS=-Dkeycloak.profile=preview \
+   -v "/home/crow/projects/keycloakify-starter/build_keycloak/src/main/resources/theme/Customized-keycloak-theme":"/opt/keycloak/themes/Customized-keycloak-theme":rw \
+   -it quay.io/keycloak/keycloak:20.0.3 \
+   start-dev
